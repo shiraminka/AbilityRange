3bea84d6750986b049f860d64bf684cb2fb9ca2d
shiraminka <kuroneko@protonmail.ch>
2019-05-26 12:31:15 +0300

    Update AbilitiesRange.lua

--- AbilitiesRange.lua
+++ AbilitiesRange.lua
@@ -1,11 +1,15 @@
 local AbilityRange = {}
 
 AbilityRange.Enable = Menu.AddOptionBool({ "NekoScripts", "Ability range" }, "Enable", false)
+AbilityRange.LocalHero = nil
+
 
 function AbilityRange.OnUpdate()
 
-Engine.ExecuteCommand("dota_range_display " .. NPC.GetAttackRange(myHero))
+Engine.ExecuteCommand("dota_range_display " .. NPC.GetAttackRange(Heroes.GetLocal()))
 
 end
+
+	
 
 return AbilityRange
